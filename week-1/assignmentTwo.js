33cd0bb0c8b9205a8e3f322afdf4e28d61f62877
destiny niesen <destiny.niesen@bryanuniversity.edu>
2022-04-23 15:43:29 -0400

    week1assignment2

---
+++ week-1/assignmentTwo.js
@@ -0,0 +1,7 @@
+console.log(document);
+
+function displayMyMessage() {
+    document.getElementById("myHeader").innerHTML = "Im so glad you made it to view my website!";
+  }
+
+  console.log(displayMyMessage());
